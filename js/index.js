a2d732322dce58cda152b396873713396f44abe4
sujeedo <67086929+sujeedo@users.noreply.github.com>
2020-11-02 11:50:10 +0900

    Add : index 배너 슬라이드 기능 js 추가
    * 추가 사항
    
    1. 이전 다음 버튼 클릭시 배너 이미지 슬라이드 기능 추가
    
    2. 자동 슬라이드 기능 추가
    
    3. 버튼 클릭시 자동 슬라이드 정지 재생 기능 추가
    
    4. 현재 배너 이미지 위치 표시 네비게이션바 자동 페인팅 기능 추가
    
    * 만들면서 느낀 점
    
    지금까지 대부분 사이트를 만들때마다 슬라이드 기능을 넣었는데, 해당 기능을 넣을때마다 조금씩 변형을 주었습니다.
    이전에는 자동 슬라이드를 멈추려면 배너에 마우스 커서를 올리기만 하면 되었습니다. 그래서 이번에는 재생 정지 버튼을 추가해보았습니다.
    또 자동 슬라이드가 되는 동안 페이지 네비게이션바를 채우는 애니메이션을 넣어서 다음 배너로 바뀌는 시간을 직관적으로 알 수도있도록 기능을 넣어보았습니다.

---
+++ js/index.js
@@ -0,0 +1,101 @@
+const sliderContainer = document.querySelector('.banner_list_container'), // 슬라이드 아이템 컨테이너
+      btnPrev = document.querySelector('.banner_prev_btn'), 
+      btnNext = document.querySelector('.banner_next_btn'); 
+      pagerContainer = document.querySelector('.paginaition');
+      pagerBtns = document.querySelectorAll('.paginaition li'); 
+let slides = document.querySelectorAll('.banner_list'), // 슬라이드 아이템
+    slideCount = slides.length, // 슬라이드 갯수
+    currentIndex = 0; // 현재 슬라이드
+    timer = undefined,
+    pagerHTML = '';
+
+// 슬라이드 이동 함수
+function goToSlide(idx) {
+  slides.forEach(slide => {
+    slide.classList.remove('visible');
+    slide.classList.remove('hidden');
+  });
+  slides[idx].classList.add('visible');
+  if(idx == 0) {
+    slides[2].classList.add('hidden');
+  }
+  if(idx == 1) {
+    slides[0].classList.add('hidden');
+  }
+  if(idx == 2) {
+    slides[1].classList.add('hidden');
+  }
+
+  currentIndex = idx;
+
+  pagerBtns.forEach(pagerBtn => {
+    pagerBtn.classList.remove('active');
+  });
+  pagerBtns[idx].classList.add('active');
+}
+goToSlide(0);
+
+// 버튼을 클릭시 슬라이드 이동
+btnPrev.addEventListener('click', () => {
+  if(currentIndex != 0) {
+    goToSlide(currentIndex - 1);
+  } else {
+    goToSlide(slideCount - 1);
+  }
+  pagePainting();
+});
+btnNext.addEventListener('click', () => {
+  if(currentIndex < slideCount - 1) {
+    goToSlide(currentIndex + 1);
+  } else {
+    goToSlide(0);
+  }
+  pagePainting();
+});
+
+// 자동 슬라이드
+function startAutoSlide() {
+  timer = setInterval(() => {
+    let nextIndex = (currentIndex + 1) % slideCount;
+    goToSlide(nextIndex);
+    pagePainting()
+  }, 4000);
+}
+startAutoSlide();
+pagePainting();
+
+// 페이지네이션 애니메이션
+function pagePainting() {
+let activePagerBtn = document.querySelector('.paginaition .active span');
+activePagerBtn.animate([
+  { width: '0%' },
+  { width: '100%' }
+], 4000);
+}
+
+function pagePaintingStop() {
+  let activePagerBtn = document.querySelector('.paginaition .active span');
+  activePagerBtn.animate([
+    { width: '100%' },
+    { width: '100%' }
+  ], 4000);
+  }
+
+const stopSlideBtn = document.querySelector('.banner_stop_btn');
+const playSlideBtn = document.querySelector('.banner_play_btn');
+stopSlideBtn.addEventListener('click', () => {
+  clearInterval(timer);
+  pagePaintingStop();
+});
+playSlideBtn.addEventListener('click', () => {
+  startAutoSlide();
+  pagePainting();
+});
+
+pagerBtns.forEach(pagerBtn => {
+  pagerBtn.addEventListener('click', (event) => {
+    let pagerNum = event.target.dataset.index;
+    goToSlide(pagerNum);
+  });
+});
+
