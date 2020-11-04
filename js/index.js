27e3d95adea209b88f1f89bc339fb05d60dac9de
sujeedo <67086929+sujeedo@users.noreply.github.com>
2020-11-04 09:45:45 +0900

    Add : index 포트폴리오 미리보기 등장 애니메이션 css, js 추가
    * 추가 사항
    
    포트폴리오 섹션에 진입하면, 시간차를 두고 포트폴리오 미리보기 이미지들이 나타났으면 좋겠다는 생각에 만들어봤습니다.
    
    for문안에 settimeout함수를 넣고 for문을 한번 돌 때마다 등장하는 애니메이션 간격이 0.3초씩 늘어나도록 조치하여 구현하였습니다.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+
+/* Home Banner */
+
 const sliderContainer = document.querySelector('.banner_list_container'), // 슬라이드 아이템 컨테이너
       btnPrev = document.querySelector('.banner_prev_btn'), 
       btnNext = document.querySelector('.banner_next_btn'); 
@@ -6,8 +9,7 @@ const sliderContainer = document.querySelector('.banner_list_container'), // 슬
 let slides = document.querySelectorAll('.banner_list'), // 슬라이드 아이템
     slideCount = slides.length, // 슬라이드 갯수
     currentIndex = 0; // 현재 슬라이드
-    timer = undefined,
-    pagerHTML = '';
+    timer = undefined;
 
 // 슬라이드 이동 함수
 function goToSlide(idx) {
@@ -15,6 +17,7 @@ function goToSlide(idx) {
     slide.classList.remove('visible');
     slide.classList.remove('hidden');
   });
+  // 부드러운 화면 전환을 위한 애니메이션을 추가합니다.
   slides[idx].classList.add('visible');
   if(idx == 0) {
     slides[2].classList.add('hidden');
@@ -25,17 +28,18 @@ function goToSlide(idx) {
   if(idx == 2) {
     slides[1].classList.add('hidden');
   }
-
+  // 현재 인덱스를 전달받은 파라미터 값으로 초기화합니다.
   currentIndex = idx;
-
+  // 페이지네이션에 슬라이드 현재 위치를 표시합니다.
   pagerBtns.forEach(pagerBtn => {
     pagerBtn.classList.remove('active');
   });
   pagerBtns[idx].classList.add('active');
 }
+// 0을 파라미터로 전달하여 슬라이드 이동 함수를 호출합니다.
 goToSlide(0);
 
-// 버튼을 클릭시 슬라이드 이동
+// 이전 버튼 클릭시 슬라이드 이동 함수가 호출됩니다.
 btnPrev.addEventListener('click', () => {
   if(currentIndex != 0) {
     goToSlide(currentIndex - 1);
@@ -44,6 +48,7 @@ btnPrev.addEventListener('click', () => {
   }
   pagePainting();
 });
+// 다음 버튼 클릭시 슬라이드 이동 함수가 호출됩니다.
 btnNext.addEventListener('click', () => {
   if(currentIndex < slideCount - 1) {
     goToSlide(currentIndex + 1);
@@ -52,19 +57,18 @@ btnNext.addEventListener('click', () => {
   }
   pagePainting();
 });
-
-// 자동 슬라이드
+// 10초 간격 자동 슬라이드 함수
 function startAutoSlide() {
   timer = setInterval(() => {
-    let nextIndex = (currentIndex + 1) % slideCount;
+    let nextIndex = (currentIndex + 1) % slideCount; // 마지막번호에선 무조건 0번으로 이동
     goToSlide(nextIndex);
     pagePainting()
   }, 10000);
 }
+// 자동 슬라이드 함수를 즉시 호출합니다.
 startAutoSlide();
-pagePainting();
 
-// 페이지네이션 애니메이션
+// 페이지네이션 재생 애니메이션 함수
 function pagePainting() {
 let activePagerBtn = document.querySelector('.paginaition .active span');
 activePagerBtn.animate([
@@ -72,7 +76,7 @@ activePagerBtn.animate([
   { width: '100%' }
 ], 10000);
 }
-
+// 페이지네이션 정지 애니메이션 함수
 function pagePaintingStop() {
   let activePagerBtn = document.querySelector('.paginaition .active span');
   activePagerBtn.animate([
@@ -80,22 +84,41 @@ function pagePaintingStop() {
     { width: '100%' }
   ], 10000);
   }
+// 페이지네이션바를 채우는 함수를 즉시 호출합니다.
+pagePainting();
 
+// 재생 버튼, 정지 버튼 요소 가져오기
 const stopSlideBtn = document.querySelector('.banner_stop_btn');
 const playSlideBtn = document.querySelector('.banner_play_btn');
+// 정지 버튼을 누르면 자동 슬라이드가 정지됩니다.
 stopSlideBtn.addEventListener('click', () => {
   clearInterval(timer);
   pagePaintingStop();
 });
+// 재생 버튼을 누르면 슬라이드가 자동 재생됩니다.
 playSlideBtn.addEventListener('click', () => {
   startAutoSlide();
   pagePainting();
 });
 
-pagerBtns.forEach(pagerBtn => {
-  pagerBtn.addEventListener('click', (event) => {
-    let pagerNum = event.target.dataset.index;
-    goToSlide(pagerNum);
-  });
-});
+/* Home Project */
+const projectPreviewSection = document.querySelector('.project'),
+      projectPreviewLists = projectPreviewSection.querySelectorAll('.project_list');
 
+function visibleIndexProject() {
+  let scrollBottomValue = document.documentElement.scrollTop + window.innerHeight;
+  let sectionTop = projectPreviewSection.offsetTop;
+  let t = 0;
+  if (sectionTop < scrollBottomValue) {
+    for (let i = 0; i < projectPreviewLists.length; i++) {
+      t += 300;
+      setTimeout(function() { 
+        projectPreviewLists[i].classList.add('visible');
+      },t);
+    }
+    // setTimeout(function () {
+    //   projectPreviewLists[1].classList.add('visible');
+    // }, 300);
+  }
+}
+window.addEventListener('scroll', throttle(visibleIndexProject, 300));
